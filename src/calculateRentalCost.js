5defdf33c225c6235687f2850adad7fade37abf6
Fábio Costa Silva <developerfabiocsilva@gmail.com>
2025-06-05 21:56:11 -0300

    finalized tranportation on vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,15 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let total = days * 40;
+
+  if (days >= 7) {
+    total -= 50; // Desconto de $50
+  } else if (days >= 3) {
+    total -= 20; // Desconto de $20
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
